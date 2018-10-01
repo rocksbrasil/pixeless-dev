28b6dbe84fe4bd840aa2394805f3daf5b64d0bfb
Rocks Brasil Team <contato@rocksbrasil.com.br>
2018-10-01 04:42:30 -0300

    up

--- defines.php
+++ defines.php
@@ -5,5 +5,5 @@ namespace pixeless;
 define('API_KEY', 'xxfyGC2EJGxkX31qrUG1CcAuaj8rh1DU');
 define('API_SECRET', '4IwWQyt6kHdg1caQk3KQPLdMXG0TIEyD');
 // valores fixos
-define('API_ENDPOINT', 'http://localhost/pixeless/api.php');//'http://www.pixeless.com.br/api.php');
+define('API_ENDPOINT', 'http://www.pixeless.com.br/api/');//'http://www.pixeless.com.br/api.php');
 
